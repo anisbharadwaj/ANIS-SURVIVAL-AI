f2abcd1ff090b14d770b0ffcba90305286708871
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-05-17 14:46:17 +0530

    Add flutter_compass package import to map_screen.dart

--- lib/presentation/screens/map/map_screen.dart
+++ lib/presentation/screens/map/map_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:flutter_compass/flutter_compass.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:http/http.dart' as http;
