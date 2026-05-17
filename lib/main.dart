28d0587c7b4a6840c41195c56e010246765f32da
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-05-17 22:55:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
-import 'presentation/screens/home/home_screen.dart';
-import 'package:flutter/material.dart';
-import 'core/theme/app_theme.dart';
+import 'package:flutter/material';
+import 'presentation/screens/map/map_screen.dart';
 
-
-void main() {
-  runApp(const SurvivalAIApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AnisSurvivalApp());
 }
 
-class SurvivalAIApp extends StatelessWidget {
-  const SurvivalAIApp({super.key});
+class AnisSurvivalApp extends StatelessWidget {
+  const AnisSurvivalApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      debugShowCheckedModeBanner: false,
-
       title: 'ANIS SURVIVAL AI',
-
-      theme: AppTheme.darkTheme,
-
-      home: const HomeScreen(),
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const MapScreen(), // 📡 Launches straight into your hardware-linked offline tracking array
     );
   }
 }
